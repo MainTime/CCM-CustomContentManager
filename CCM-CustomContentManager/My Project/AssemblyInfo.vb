f91352bcf796cc3a10a920df0df9cb1d712bd894
MainTime <YTMainTime@gmail.com>
2019-06-03 15:46:15 +0200

    Projektdateien hinzufügen.

---
+++ CCM-CustomContentManager/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("CCM-CustomContentManager")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CCM-CustomContentManager")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("8344c332-4433-41f1-a6c1-cd99fb11be28")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
